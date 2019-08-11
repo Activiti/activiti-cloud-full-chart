3310c38ebc4133b6a17bc75633898d5ca555426a
Igor Dianov <igor@dianov.com>
2019-08-11 12:40:11 -0700

    fix: update release scripts

--- charts/activiti-cloud-full-example/release.sh
+++ charts/activiti-cloud-full-example/release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export HELM_ACTIVITI_VERSION=7.1.0-M2
-export APP_ACTIVITI_VERSION=7.1.0.M2
+export HELM_ACTIVITI_VERSION=7.1.0-M3.1
+export APP_ACTIVITI_VERSION=7.1.0.M3.1
 export GITHUB_CHARTS_REPO="https://github.com/Activiti/activiti-cloud-helm-charts.git"
 
-
 make printrelease
-sed -i -e "s/#tag: .*/tag: $APP_VERSION/" values.yaml
+sed -i -e "s/appVersion: .*/appVersion: $APP_ACTIVITI_VERSION/" Chart.yaml
+sed -i -e "s/#tag: .*/tag: $APP_ACTIVITI_VERSION/" values.yaml
 make tag
 make release
 make github
